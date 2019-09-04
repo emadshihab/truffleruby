36c37f05d59da1d8ff2ace270919b8400b594904
Chris Seaton <chris.seaton@shopify.com>
2019-09-04 12:58:50 -0400

    Run Liquid tests in CI

---
+++ truffle/test-liquid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+source truffle/test-common.sh
+
+git clone git@github.com:Shopify/liquid.git
+pushd liquid
+
+gem install minitest -v 5.11.3
+rake base_test
+
+popd
