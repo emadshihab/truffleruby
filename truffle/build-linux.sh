aa0e067bbc37e0aa8b4ca60d595c52e0f94f3128
Jean Boussier <jean.boussier@gmail.com>
2019-09-23 17:31:38 +0200

    Get rid of the Dockerfile COPY

--- truffle/build-linux.sh
+++ truffle/build-linux.sh
@@ -7,11 +7,14 @@ home_prefix=
 platform=linux
 bootstrap_java_home=/usr/lib/jvm/java-8-openjdk-amd64
 
+parent_path=$(realpath "$(dirname -- "$(readlink -f -- "$BASH_SOURCE[0]")")")
+echo $parent_path
+
 build_dir=$PWD/$(mktemp -d temp-build-XXXXXXXXXX)
 pushd $build_dir
 
-source ../build-common.sh
+source "${parent_path}/build-common.sh"
 
-cp $shopify_name.tar.gz $shopify_name.tar.gz.sha256 ../artifacts
+cp $shopify_name.tar.gz $shopify_name.tar.gz.sha256 /build/artifacts
 
 popd
