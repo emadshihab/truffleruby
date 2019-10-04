b0c717406e82ed739663f7dedfbb4c76229aefb9
Rafael Mendonça França <rafael@franca.dev>
2019-10-04 17:58:18 -0400

    Remove llvm from the build machines

--- .shopify-build/setup-macos.sh
+++ .shopify-build/setup-macos.sh
@@ -3,8 +3,5 @@
 set -x
 set -e
 
-# Install TruffleRuby dependencies
-brew install openssl llvm@4
-
 # Install dependencies for tests - we'll just install all in all cases at the moment
 brew install libxml2 libxslt memcached pkgconfig sqlite3
