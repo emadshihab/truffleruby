771f75df8725bc85b576eb2e096e9c0d7bfdfaff
Chris Seaton <chris.seaton@shopify.com>
2019-11-04 08:59:43 -0500

    Update import

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "0f5609a467b40e9888984a57295af42f710db611",
+                "version": "95ebf197a6fc312acaba2fd38a4f36738cce2258",
                 "urls": [
                     {"url": "git@github.com:Shopify/graal-shopify.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "0f5609a467b40e9888984a57295af42f710db611",
+                "version": "95ebf197a6fc312acaba2fd38a4f36738cce2258",
                 "urls": [
                     {"url": "git@github.com:Shopify/graal-shopify.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
