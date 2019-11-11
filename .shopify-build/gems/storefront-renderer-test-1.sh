a7ecd28458832b0e69d85bcc8ee83a4330ba39b2
Chris Seaton <chris.seaton@shopify.com>
2019-11-11 19:24:58 +0000

    Tidy library load workaround

--- .shopify-build/gems/storefront-renderer-test-1.sh
+++ .shopify-build/gems/storefront-renderer-test-1.sh
@@ -3,8 +3,4 @@ cd storefront-renderer
 sudo apt-get update
 sudo apt-get install -y mysql-client
 
-# Sulong library load workaround
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/build/truffleruby-shopify/.shopify-build/build/jre/languages/ruby/lib/cext/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/build/truffleruby-shopify/.shopify-build/build/jre/languages/llvm/native/lib/
-
 bundle exec rake db:load test:unit test:shared test:liquid
--- .shopify-build/setup-linux.sh
+++ .shopify-build/setup-linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# Sulong library load workaround
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/build/truffleruby-shopify/.shopify-build/build/jre/languages/ruby/lib/cext/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/build/truffleruby-shopify/.shopify-build/build/jre/languages/llvm/native/lib/
