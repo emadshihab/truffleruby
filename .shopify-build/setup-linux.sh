d0c7f7f2d5360eceb731a6bef8c4e0b33911a9bb
Chris Seaton <chris.seaton@shopify.com>
2019-11-12 12:22:29 +0000

    Library workaround no longer needed

--- .shopify-build/setup-linux.sh
+++ .shopify-build/setup-linux.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# Sulong library load workaround
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/build/truffleruby-shopify/.shopify-build/build/jre/languages/ruby/lib/cext/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/build/truffleruby-shopify/.shopify-build/build/jre/languages/llvm/native/lib/
+# No setup needed at the moment
